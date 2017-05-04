563bda86defbf36ec8077a91583cd132eea3bc53
mrbolae <michael.burkard@gmx.ch>
2017-05-04 14:09:48 +0200

    reduced sleeps between each stone to the shortest time possible

--- test-stones-even-closer-together.sh
+++ test-stones-even-closer-together.sh
@@ -25,7 +25,7 @@ run_blue_stone () {
     sleep .3
     echo "valve1=off"
     echo "lightbarrier3=on"
-    sleep .45
+    sleep 1
     echo "lightbarrier3=off"
 }
 
@@ -43,7 +43,7 @@ run_red_stone () {
     sleep .3
     echo "valve2=off"
     echo "lightbarrier4=on"
-    sleep .45
+    sleep 1
     echo "lightbarrier4=off"
 }
 
@@ -61,25 +61,38 @@ run_white_stone () {
     sleep .3
     echo "valve3=off"
     echo "lightbarrier5=on"
-    sleep .45
+    sleep 1
     echo "lightbarrier5=off"
 }
 
-echo "Setting up the hardware in 5 secs" >&2
-sleep 5
+run_undetected_stone () {
+    echo "lightbarrier1=on"
+    sleep .1
+    echo "lightbarrier1=off"
+    echo "color=transparent"
+    sleep 1.56
+    echo "lightbarrier2=on"
+    sleep .45
+    echo "lightbarrier2=off"
+}
+
+echo "Setting up the hardware in 2 secs" >&2
+sleep 2
 echo "Set up the hardware now..." >&2
 start_hardware &
 
-echo "Waiting 10 secs before we begin with the first stone..." >&2
-sleep 10
+echo "Waiting 1 secs before we begin with the first stone..." >&2
+sleep 1
 echo "...and here we go..." >&2
 
 run_blue_stone &
-sleep 3
+sleep 1.7
 run_red_stone &
-sleep 3
+sleep 1.7
 run_white_stone &
-sleep 10
+sleep 1.7
+run_undetected_stone &
+sleep 6
 
 echo "Finished" >&2
 echo "Stop the hardware" >&2
