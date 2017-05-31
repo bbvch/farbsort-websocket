04fe759ebab65bf17abc6efedd880ff970c2bc22
mrbolae <michael.burkard@gmx.ch>
2017-05-31 10:37:51 +0200

    ajusted times according to measured times.

--- test-stones-as-fast-as-lightning.sh
+++ test-stones-as-fast-as-lightning.sh
@@ -15,15 +15,17 @@ run_blue_stone () {
     echo "lightbarrier1=on"
     sleep .1
     echo "lightbarrier1=off"
+    sleep 1.5
     echo "color=blue"
-    sleep 1.56
+    sleep 1.5
     echo "lightbarrier2=on"
-    sleep .45
+    sleep .1
     echo "lightbarrier2=off"
-    sleep .24
+    sleep .4
     echo "valve1=on"
-    sleep .3
+    sleep .1
     echo "valve1=off"
+    sleep .1
     echo "lightbarrier3=on"
     sleep 1
     echo "lightbarrier3=off"
@@ -33,15 +35,17 @@ run_red_stone () {
     echo "lightbarrier1=on"
     sleep .1
     echo "lightbarrier1=off"
+    sleep 1.5
     echo "color=red"
-    sleep 1.56
+    sleep 1.5
     echo "lightbarrier2=on"
-    sleep .45
+    sleep .1
     echo "lightbarrier2=off"
-    sleep 1.42
+    sleep 1.3
     echo "valve2=on"
-    sleep .3
+    sleep .1
     echo "valve2=off"
+    sleep .1
     echo "lightbarrier4=on"
     sleep 1
     echo "lightbarrier4=off"
@@ -51,15 +55,17 @@ run_white_stone () {
     echo "lightbarrier1=on"
     sleep .1
     echo "lightbarrier1=off"
+    sleep 1.5
     echo "color=white"
-    sleep 1.56
+    sleep 1.5
     echo "lightbarrier2=on"
-    sleep .45
+    sleep .1
     echo "lightbarrier2=off"
-    sleep 2.19
+    sleep 2.2
     echo "valve3=on"
-    sleep .3
+    sleep .1
     echo "valve3=off"
+    sleep .1
     echo "lightbarrier5=on"
     sleep 1
     echo "lightbarrier5=off"
@@ -69,10 +75,11 @@ run_undetected_stone () {
     echo "lightbarrier1=on"
     sleep .1
     echo "lightbarrier1=off"
+    sleep 1.5
     echo "color=transparent"
-    sleep 1.56
+    sleep 1.5
     echo "lightbarrier2=on"
-    sleep .45
+    sleep .1
     echo "lightbarrier2=off"
 }
 
