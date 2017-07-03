f2aca0e36f21ba33f7e1f0065845b1588011a497
Dominik Berner <dominik.berner@@bbv.ch>
2017-07-03 09:14:15 +0200

    corrected python tafs and added link for fcntl

--- setup.py
+++ setup.py
@@ -98,8 +98,8 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+		'hwcontrol:sys_platform=="linux"' : ['fcntl']
+
     },
 
     # If there are data files included in your packages that need to be
